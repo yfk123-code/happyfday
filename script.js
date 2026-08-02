bff795bd4a34365f4ddad0ff0a8c64106f0c1853
yfk123-code <75697534+yfk123-code@users.noreply.github.com>
2026-08-02 16:12:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const sheetURL = 'https://script.google.com/macros/s/AKfycbwdj4tTL3JYTCB9Q33NnByXWq9KS93CIdYNy5RFGxe_mZ44vyNvi-iAqSoB3p-RicnG/exec'; 
+const sheetURL = 'https://script.google.com/macros/s/AKfycbzspwMl7IHVUOOVakoE6TkpHwjf1uv6MGUqBmjSBr49uwKkbXY-nuJZANik1krP5h-x/exec'; 
 let uData = { name: "", image: "", id: "FD-2026-" + Math.floor(100000 + Math.random() * 899999) };
 
 function nextStep(s) {
